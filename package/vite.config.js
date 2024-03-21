88bf120d3eee3216e344f989cf2462d50d49ad5f
yaolifeng <y15829485647@163.com>
2024-03-21 20:43:31 +0800

    logue: change build package path

--- package/vite.config.js
+++ package/vite.config.js
@@ -6,21 +6,19 @@ export default defineConfig({
     open: true,
     plugins: [vue(), libCss()],
     build: {
-        outDir: 'ContributionMap', //输出文件名称
+        outDir: 'dist/contribution-map',
         lib: {
-            entry: path.resolve(__dirname, './src/components/index.js'), //指定组件编译入口文件
-            name: 'ContributionMap',
-            fileName: 'ContributionMap',
-        }, //库编译模式配置
+            entry: path.resolve(__dirname, './src/components/index.js'),
+            name: 'contribution-map',
+            fileName: 'contribution-map',
+        },
         rollupOptions: {
-            // 确保外部化处理那些你不想打包进库的依赖
             external: ['vue'],
             output: {
-                // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
                 globals: {
                     vue: 'Vue',
                 },
             },
-        }, // rollup打包配置
+        },
     },
 });
